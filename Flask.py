586d711baef50904244aad6098b6329eba88994e
AlexSG <alexanderstopper@hotmail.com>
2022-06-26 12:14:12 -0500

    Inicio de aplicacion

---
+++ Flask.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+# Inicializar la aplicacion
+app = Flask(__name__, template_folder='templates\layouts')
+
+
+#Ruta para llamara a la pagina principal.
+@app.route('/')
+def principal():
+    return render_template('Principal.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) # Para ejecutar la aplicacion
