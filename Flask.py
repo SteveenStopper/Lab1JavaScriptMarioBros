d67621d03cc3a52a8b9e9bdbf5a4981258db6dc5
AlexSG <alexanderstopper@hotmail.com>
2022-06-26 12:40:43 -0500

    avance de la aplicacion

--- Flask.py
+++ Flask.py
@@ -9,5 +9,10 @@ app = Flask(__name__, template_folder='templates\layouts')
 def principal():
     return render_template('Principal.html')
 
+#Ruta para llamara a la pagina del juego.
+@app.route('/Juego')
+def juegoMB():
+    return render_template('JuegoMarioB.html')
+
 if __name__ == '__main__':
     app.run(debug=True) # Para ejecutar la aplicacion
